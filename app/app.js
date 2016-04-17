b06fe015e2a4e44f5e4a0f108e59da332728039f
pekmil <pekmil@freemail.hu>
2016-04-17 21:52:25 +0200

    Initial commit - cleanup

--- app/app.js
+++ app/app.js
@@ -1,12 +1,101 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
+var tmwcapp = angular.module('TriathlonManagerWebClient', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ui.bootstrap',
+  'ngWebSocket',
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+  $routeProvider
+  	.when('/tournaments', {
+      templateUrl: 'views/partial/tournament.html',
+      controller: 'TournamentCtrl'
+    })
+    .when('/races', {
+      templateUrl: 'views/partial/race.html',
+      controller: 'RaceCtrl'
+    })
+    .when('/races/:tournamentId', {
+      templateUrl: 'views/partial/race.html',
+      controller: 'RaceCtrl'
+    })
+    .when('/entries/:raceId', {
+      templateUrl: 'views/partial/entry.html',
+      controller: 'EntryCtrl'
+    })
+    .when('/results/:raceId', {
+      templateUrl: 'views/partial/result.html',
+      controller: 'ResultCtrl'
+    })
+    .when('/raceresults/:raceId', {
+      templateUrl: 'views/partial/raceresult.html',
+      controller: 'EntryCtrl'
+    })
+    .when('/invoices/:raceId', {
+      templateUrl: 'views/partial/invoice.html',
+      controller: 'InvoiceCtrl'
+    })
+    .when('/familyentries/:raceId', {
+      templateUrl: 'views/partial/familyentry.html',
+      controller: 'EntryCtrl'
+    })
+    .when('/parameters', {
+      templateUrl: 'views/partial/parameter.html',
+      controller: 'ParameterCtrl'
+    })
+    .when('/admin', {
+      templateUrl: 'views/partial/admin.html',
+      controller: 'AdminCtrl'
+    })
+    .when('/licences', {
+      templateUrl: 'views/partial/licence.html',
+      controller: 'LicenceCtrl'
+    })
+  	.otherwise({
+      templateUrl: 'views/partial/welcome.html',
+      controller: 'WelcomeCtrl'
+    }
+  	);
+
+	$httpProvider.defaults.useXDomain = true;
+  $httpProvider.defaults.withCredentials = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  $httpProvider.interceptors.push('httpInterceptor');
+}
+]).
+run(['WSNotification', function(WSNotification) {
+  //init the notification service
+}]).
+constant('AppConfig',
+  {
+    entryFees : {
+      "Újonc" : {
+        pre : 2000,
+        normal : 4000
+      },
+      "Gyermek" : {
+        pre : 4000,
+        normal : 6000
+      },
+      "Serdülő" : {
+        pre : 4000,
+        normal : 6000
+      },
+      "Sprint" : {
+        pre : 6000,
+        normal : 9000
+      },
+      "Rövid" : {
+        pre : 8000,
+        normal : 11000
+      }
+    },
+    carouselImages : [
+      {src : 'resources/images/carousel/triathlon-swim.jpg', caption : 'Úszás', desc : ''},
+      {src : 'resources/images/carousel/triathlon-bicycle.jpg', caption : 'Biciklizés', desc : ''},
+      {src : 'resources/images/carousel/triathlon-run.jpg', caption : 'Futás', desc : ''}
+    ],
+    serviceBaseURL : 'http://192.168.1.103:8080/TriathlonManager/rest/',
+    notificationWSEndpoint : 'ws://192.168.1.103:8080/TriathlonManager/notification'
+  }).
+value('version', '0.1');
